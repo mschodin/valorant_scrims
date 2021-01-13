2096a9de7ac2f77f1829a0dc19a964bbe9552333
mschodin <michael-schodin@uiowa.edu>
2021-01-13 13:15:44 -0600

    initial commit of rails app with javascript

---
+++ app/javascript/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>
+                    You are on the home page!
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
