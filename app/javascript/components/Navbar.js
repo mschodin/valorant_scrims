2f5a815294a6244f3f5e778b4d541c1f29fca778
mschodin <michael-schodin@uiowa.edu>
2021-01-18 14:35:23 -0600

    Added login button to nav bar

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import sg_logo from '/app/assets/images/sg_logo.png';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function NavBar() {
     return (
         <>
-            <Navbar bg="primary">
+            <Navbar bg="primary" variant={'dark'}>
                 <Navbar.Brand href="#home">
                     <img
                         src={sg_logo}
@@ -15,6 +18,16 @@ function NavBar() {
                         alt="Scrub Garden Logo"
                     />
                 </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href={"#schedule"}>Schedule</Nav.Link>
+                    <Nav.Link href={"#teams"}>Teams</Nav.Link>
+                    <Nav.Link href={"#profile"}>Profile</Nav.Link>
+                    <Nav.Link href={"#messages"}>Messages</Nav.Link>
+                    <Nav.Link href={"#your_scrims"}>Your Scrims</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <Button variant={'dark'}>Login</Button>
+                </Form>
             </Navbar>
         </>
     );
