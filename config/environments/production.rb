fa72cc440789a43666a67238ec4d1ba688038fbc
mschodin <michael-schodin@uiowa.edu>
2021-01-21 14:06:33 -0600

    Updated production mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,13 +95,13 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "example.com",
+      domain: "gmail.com",
       authentication: "plain",
       enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password:ENV["GMAIL_PASSWORD"]
+      user_name: ENV["VAL_GMAIL_USER"],
+      password:ENV["VAL_GMAIL_PASS"]
   }
   # doesn't have to be Heroku, but you get the idea.
-  config.action_mailer.default_url_options = { :host => 'yourhostsite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'scrub-garden-valorant-scrims.herokuapp.com' }
 
 end
